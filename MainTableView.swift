b0ba7cae07532fad5be19414af1b19b0935cb690
pos <pos@CTI-IFSPs-Mac-mini-85.local>
2019-09-20 22:09:58 -0300

    Tela principal

---
+++ MainTableView.swift
@@ -0,0 +1,21 @@
+//
+//  MainTableView.swift
+//  PhoneBook
+//
+//  Created by pos on 20/09/2019.
+//  Copyright © 2019 br.com.ifsp.minguini. All rights reserved.
+//
+
+import UIKit
+
+class MainTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
