3d9fab38e4814fc3d4794f54195f0782d8f223cf
Matheus minguini <pos@CTI-IFSPs-Mac-mini-85.local>
2019-11-22 21:18:23 -0300

    Finalização do CRUD usando CoreData

--- PhoneBook/ViewController.swift
+++ PhoneBook/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by pos on 13/09/2019.
 //  Copyright © 2019 br.com.ifsp.minguini. All rights reserved.
 //
-
 import UIKit
 import CoreData
 import Firebase
@@ -19,74 +18,105 @@ class ViewController: UIViewController {
     @IBOutlet weak var intimidade: UITextField!
     
     var contato : Contato!
-    var istoEdit: Bool!
+    var isEditar : Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
         if(contato != nil) {
             nome.text = contato.nome
             sobrenome.text = contato.sobrenome
             intimidade.text = String(contato.intimidade)
-            
-            self.istoEdit = true
+            celular.text = contato.numero
+            self.isEditar = true
         }
-        
-        
-        //Conexao com o Banco remoto do FireBase
-        let remoteDataBase = Database.database().reference()
-        remoteDataBase.child("contato").setValue(100)
     }
-
+    
     @IBAction func inserirContato(_ sender: Any) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
+        let success : Bool
         
-        let contato = NSEntityDescription.insertNewObject(forEntityName: "Contato", into:context)
+        if(self.isEditar){
+            success = editar(appDelegate : appDelegate);
+        }else{
+            success = salvar(appDelegate: appDelegate);
+        }
         
-        contato.setValue(nome.text,forKey:"nome")
-        contato.setValue(sobrenome.text, forKey: "sobrenome")
-        
-        
-        let enteredCountryCode = String(pais.text!)
-        let enteredStateCode = String(ddd.text!)
-        let enteredCellPhone = String(celular.text!)
-        let celular_completo = enteredCountryCode + " " + enteredStateCode + " " + enteredCellPhone
-        
-        let enteredIntimacy =  String(intimidade.text!)
-        let intimacyAsNumber = (enteredIntimacy as NSString).integerValue
-        
-        contato.setValue(celular_completo, forKey: "numero")
-        contato.setValue(intimacyAsNumber, forKey: "intimidade")
-        
-        let enteredName = String(nome.text!)
-        
-        do {
-            try
-                context.save()
-                createCustumeMessage(message: "O contato " + enteredName + " foi salvo com sucesso na base local e remota")
-            } catch{
-                createCustumeMessage(message: "O contato " + enteredName + " NÃO foi salvo. Por favor, retente em alguns minutos.")
-            }
+        if(success){
+            createCustumeMessage(message: "O contato " + nome.text! + " foi registrado com sucesso na base local e remota")
+        }else{
+            createCustumeMessage(message: "O contato " + nome.text! + " NÃO foi registrado. Por favor, retente em alguns minutos.")
+        }
     }
     
+    func salvar(appDelegate : AppDelegate) -> Bool{
+        let context = appDelegate.persistentContainer.viewContext
+        let contato = NSEntityDescription.insertNewObject(forEntityName: "Contato", into:context)
+        
+        setContatoValues(contatoBD: contato)
+        
+        do {
+            try context.save()
+        } catch {
+            return false
+        }
+ 
+        return true
+    }
     
+    func editar(appDelegate : AppDelegate) -> Bool{
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Contato")
+        
+        fetchRequest.predicate = NSPredicate(format: "nome = %@", self.contato.nome ?? "")
+        
+        do {
+            let resultado = try context.fetch(fetchRequest)
+            let contatoUpdate = resultado[0] as! NSManagedObject
+            
+            setContatoValues(contatoBD: contatoUpdate)
+            
+            try context.save()
+        } catch {
+           return false
+        }
+        return true
+    }
+    
+    func setContatoValues(contatoBD : NSManagedObject){
+        let celular_completo = buildCelularCompleto()
+        let intimidade = buildIntimidade()
+        
+        contatoBD.setValue(nome.text, forKey:"nome")
+        contatoBD.setValue(sobrenome.text, forKey: "sobrenome")
+        contatoBD.setValue(celular_completo, forKey: "numero")
+        contatoBD.setValue(intimidade, forKey: "intimidade")
+    }
     
     func createCustumeMessage(message : String){
         
-        let alertAction = UIAlertAction( title : "Ok" ,
-                                         style : .cancel) { action in
-                                            (self.navigationController?.popToRootViewController(animated: true));
+        let alertAction = UIAlertAction( title : "Ok", style : .cancel) {
+            action in (self.navigationController?.popToRootViewController(animated: true));
         }
         
-        let alert = UIAlertController(title: "Aviso",
-                                      message: message,
-                                      preferredStyle: .alert)
+        let alert = UIAlertController(title: "Aviso", message: message, preferredStyle: .alert)
         
         alert.addAction(alertAction)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    func buildCelularCompleto() -> String{
+        let enteredCountryCode = String(pais.text!)
+        let enteredStateCode = String(ddd.text!)
+        let enteredCellPhone = String(celular.text!)
+        
+        return enteredCountryCode + " " + enteredStateCode + " " + enteredCellPhone
+    }
+    
+    func buildIntimidade() -> Int{
+        let enteredIntimacy =  String(intimidade.text!)
+        return (enteredIntimacy as NSString).integerValue
     }
 }
 
